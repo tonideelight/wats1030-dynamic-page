b483472ae29f3c39ddfad3a9e442e2b7dd7a6e80
Toni Dee <deea@seattleu.edu>
2017-07-27 12:19:01 +0000

    add flickr API to new subdomain

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
+
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = 43b98dea2e554c00612f0336aea5e8a3; // This will be your Flickr-issued API Key.
+$PAGE_TITLE = 'Avocados'; // This should be a user-friendly page title.
+$SEARCH_TAGS = 'avocado'; // This must be a comma-separated list
 
 ?>
